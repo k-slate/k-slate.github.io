5781afb10b7deca400a9f11403fb7817e3cc09aa
k-slate <103887423+k-slate@users.noreply.github.com>
2022-05-06 11:03:45 +0900

    Create word_check.js

---
+++ study/study/geography/iGeo/word_check/word_check.js
@@ -0,0 +1,88 @@
+onload = function() {
+    main();
+}
+
+const dictionaly = [
+[
+["地殻", "crust"],
+["外核", "outer core"],
+["内核", "inner core"],
+["大陸地殻", "continental crust"],
+["海洋地殻", "oceanic crust"],
+["プレート境界", "boundary"],
+["プレート境界", "plate margin"],
+["狭まる境界", "destructive margin"],
+["広がる境界", "constructive margin"],
+["ずれる境界", "conservative margin"],
+["海溝", "ocean trench"],
+["半球", "hemisphere"],
+["赤道", "equator"],
+["更新世の", "Pleistocene"],
+["氷河期", "glacial period"],
+["北回帰線", "Tropic of Cancer"],
+["南回帰線", "Tropic of Capricorn"],
+["成層圏", "stratosphere"],
+["地軸", "the earth’s axis"],
+["赤道収束帯", "inter-tropical convergence zone"],
+["亜熱帯高圧帯", "subtropical zone of high pressure"],
+["コリオリの力", "Coriolis force"],
+["大圏航路", "orthodrome"],
+["春分・秋分", "equinox"],
+["海水面変動の", "eustatic"],
+["アセノスフェア", "asthenosphere"],
+["リソスフェア", "lithosphere"],
+["狭まる境界", "convergent boundary"],
+["広がる境界", "divergent boundary"],
+["(プレートが) 沈み込む", "subduct"],
+["スラブ", "slab"],
+["ファンデフカプレート", "Juan de Fuca plate"],
+["ココスプレート", "Cocos plate"],
+["南極プレート", "Antarctic plate"],
+["インドプレート", "Indian plate"],
+["オーストラリアプレート", "Australian plate"],
+["南アメリカプレート", "South American plate"],
+["ユーラシアプレート", "Eurasian plate"],
+["ナスカプレート", "Nazca plate"],
+["カリブプレート", "Caribbean plate"],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+["", ""],
+],
+[
+
+],
+[
+
+],
+[
+
+],
+[
+
+],
+[
+
+],
+[
+
+],
+[
+
+],
+[
+
+],
+[
+
+],
+];
