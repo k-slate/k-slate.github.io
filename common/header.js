03e19ad5deb728dd705234bdab73b70628967a65
k-slate <103887423+k-slate@users.noreply.github.com>
2022-10-26 23:46:50 +0900

    Add files via upload

---
+++ common/header.js
@@ -0,0 +1,18 @@
+﻿function tight_display (open_button) {
+    open_button.style.display = "none";
+    let close_button = document.getElementsByClassName("close_button")[0];
+    close_button.style.display = "block";
+    
+    let menu = window.parent.document.getElementsByClassName("menu")[0];
+    menu.style.display = "block";
+}
+
+
+function tight_clear (close_button) {
+    close_button.style.display = "none";
+    let open_button = document.getElementsByClassName("open_button")[0];
+    open_button.style.display = "block";
+    
+    let menu = window.parent.document.getElementsByClassName("menu")[0];
+    menu.style.display = "none";
+}
