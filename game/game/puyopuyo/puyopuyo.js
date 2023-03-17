bc2cf9ddc6e1766613e3efeb7406e91c81046920
k-slate <103887423+k-slate@users.noreply.github.com>
2023-03-17 15:33:51 +0900

    Update puyopuyo.js

--- game/game/puyopuyo/puyopuyo.js
+++ game/game/puyopuyo/puyopuyo.js
@@ -186,10 +186,8 @@ function main(){
     const sq_height = canvas.height  - cell_left * 2;
     const sq_size = sq_width / cell_size_x;
     let border = document.defaultView.getComputedStyle(canvas, null).getPropertyValue("border-width").slice(0, -2) - 0;
-    sub_canvas.width = 4 * sq_size + cell_top * 2;
-    sub_canvas.height = 3 * sq_size + cell_left * 2;
-    sub_canvas.style.width = (sub_canvas.width / magnification) + "px";
-    sub_canvas.style.height = (sub_canvas.height / magnification) + "px";
+    sub_canvas.width = sub_canvas.clientWidth * magnification;
+    sub_canvas.height = sub_canvas.clientHeight * magnification;
     const pallet = ["#00000000", "#ffffffff", "#ff3333aa", "#00cc00aa",  "#4444ffaa", "#cccc00aa"];
     
     
