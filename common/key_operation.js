f207040819e9e144b89c0a6ec6f0e34b2011ad40
k-slate <103887423+k-slate@users.noreply.github.com>
2023-06-28 23:31:11 +0900

    Update key_operation.js

--- common/key_operation.js
+++ common/key_operation.js
@@ -1,4 +1,9 @@
-function key_operation (key_code) {
+function virtual_keydown (key_code) {
     let e = new KeyboardEvent("keydown", {key: key_code});
+    document.dispatchEvent(e);
+}
+
+function virtual_keyup () {
+    let e = new KeyboardEvent("keyup");
     document.dispatchEvent(e);
 }
